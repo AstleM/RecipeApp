3a5d99c98d94f768d837e4617ff800f8e9f45bd3
Mike <mtastle@hotmail.com>
2022-08-04 21:29:01 +0100

    Added base repos

---
+++ RecipeApp.API/Contracts/Repos/IBaseRepo.cs
@@ -0,0 +1,12 @@
+﻿namespace RecipeApp.API.Contracts.Repos
+{
+    public interface IBaseRepo<T> where T:class
+    {
+        Task<T> GetAsync(int id);
+        Task<List<T>> GetAllAsync();
+        Task<T> CreateAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(int id);
+        Task<bool> ExistsAsync(int id);
+    }
+}
---
+++ RecipeApp.API/Repos/BaseRepo.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RecipeApp.API.Contracts.Repos;
+using RecipeApp.API.Data;
+
+namespace RecipeApp.API.Repos
+{
+    public class BaseRepo<T> : IBaseRepo<T> where T : class
+    {
+        private readonly RecipeDbContext context;
+
+        public BaseRepo(RecipeDbContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<T> CreateAsync(T entity)
+        {
+            await context.AddAsync(entity);
+            await context.SaveChangesAsync();
+            return entity;
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            T entity = await context.Set<T>().FindAsync(id);
+            context.Set<T>().Remove(entity);
+            await context.SaveChangesAsync();
+        }
+
+        public async Task<bool> ExistsAsync(int id)
+        {
+            T entity = await context.Set<T>().FindAsync(id);
+            return entity != null;
+        }
+
+        public async Task<List<T>> GetAllAsync()
+        {
+            return await context.Set<T>().ToListAsync();
+        }
+
+        public async Task<T> GetAsync(int id)
+        {
+            return await context.Set<T>().FindAsync(id);
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            context.Update(entity);
+            await context.SaveChangesAsync();
+        }
+    }
+}
